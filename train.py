325caeada91eebe0138875f02977a6c885fb67f5
Sebby11 <used4sebastianid@gmail.com>
2021-08-07 18:03:51 -0700

    stuck at 70% loss

--- train.py
+++ train.py
@@ -22,11 +22,18 @@ from PIL import Image
 
 from catsdogsdataset import CatsDogsDataset
 
+transform = transforms.Compose([
+				transforms.ToTensor(),
+				transforms.Normalize([0.5,0.5,0.5],
+									[0.5,0.5,0.5])
+			])
 # 1.) Get Data & place it to batch load w/ DataLoader
 training_data = CatsDogsDataset(csv='cats_dogs.csv', image_dir='data\\train\\both', 
-								transform=transforms.ToTensor())
+								transform=transform)
 
-train_loader = DataLoader(dataset=training_data, batch_size=100, shuffle=True)
+train, val = torch.utils.data.random_split(training_data, [1000, 19000])
+
+train_loader = DataLoader(dataset=train, batch_size=10, shuffle=True)
 
 '''
 # Test print an image from the data
@@ -34,40 +41,107 @@ images, labels = next(iter(train_loader))
 
 print("Images: \n", images, "\n\n")
 print("Labels: ", labels, "\n\n")
-plt.imshow(images[2].permute(1, 2, 0))
-plt.show()
+for i in range(10):
+	plt.imshow(images[i].permute(1, 2, 0))
+	plt.show()
+exit()
 '''
 
+
 # 2.) Get model // ResNet
-class ResNet(nn.Module):
+class network(nn.Module):
 	def __init__(self):
 		super().__init__()
-		self.layer1 = nn.Linear(250 * 250, 64)
-		self.layer2 = nn.Linear(64, 64)
-		self.layer3 = nn.Linear(64, 10)
-		self.do     = nn.Dropout(0.1)
+		self.l1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5)
+		self.l2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+		self.l3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5)
+
+		self.maxPooling = nn.MaxPool2d(kernel_size=4)
+		self.adaptive = nn.AdaptiveAvgPool1d(256)
+
+		self.l4 = nn.Linear(in_features=256, out_features=128)
+		self.l5 = nn.Linear(in_features=128, out_features=64)
+		self.out = nn.Linear(in_features=64, out_features=1)
 
 	def forward(self, x):
-		hiddel_Layer1 = nn.functional.relu(self.layer1(x))
-		hiddel_Layer2 = nn.functional.relu(self.layer2(h1))
-		dropout       = self.do(h2 + h1)
-		prediction	  = self.layer3(dropout)
-		return prediction
-model = ResNet()
+		x = self.l1(x)
+		#max pooling layer
+		x = self.maxPooling(x)
+		x = nn.functional.relu(x)
+		
+		x = self.l2(x)
+		x = self.maxPooling(x)
+		x = nn.functional.relu(x)
+
+		x = self.l3(x)
+		x = self.maxPooling(x)
+		x = nn.functional.relu(x)
+
+		x = nn.functional.dropout(x)
+		x = x.view(1, x.size()[0], -1)
+		x = self.adaptive(x).squeeze()
+
+		x = self.l4(x)
+		x = nn.functional.relu(x)
+
+		x = self.l5(x)
+		x = nn.functional.relu(x)
+
+		x = nn.functional.relu(self.out(x))
+
+		return x
+	
+model = network()
+
 
 # 3.) Get optimizer
 optimizer = optim.SGD(model.parameters(), lr=1e-2)
 
 # 4.) Get loss(objective) function
-loss_function = nn.CrossEntropyLoss()
+loss_function = nn.BCELoss()
 
 
 # 5.) Training Loop
-for epoch in range(100):
-	losses = list()
-
+for epoch in range(2):
+	total_loss = 0.0
+	cnt = 1
 	for batch in train_loader:
-		inputImage, label = batch
-		print("X: ", inputImage.size())
-		print("Y: ", label)
-		exit()
+		inputImages, labels = batch
+
+		labels = labels.view(10, 1)
+		labels = labels.float()
+		#inputImages = inputImages.view(inputImages.size(0), -1)
+		#print("INPUT IMAGES: ", inputImages.size())
+		
+		
+		prediction = model(inputImages)
+		#exit()
+
+		loss = loss_function(prediction, labels)
+		#print(prediction)
+		#print(labels)
+		#print()
+		print("Loss {}: {}".format(cnt, loss))
+		cnt += 1
+		#exit()
+
+		optimizer.zero_grad()
+
+		loss.backward()
+
+		optimizer.step()
+
+		total_loss += loss.item()
+
+	print("Epoch {}: Loss {}".format(epoch, total_loss))
+
+images, labels = next(iter(train_loader))
+
+print("Images: \n", images[2], "\n\n")
+print("Labels: ", labels[2], "\n\n")
+
+prediction = model(images)
+print("PREDICTION: ", prediction)
+
+plt.imshow(images[2].permute(1, 2, 0))
+plt.show()
